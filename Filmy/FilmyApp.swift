5a5c97c99c65126ad2e7155111ca2096c39bf7ba
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-05-29 09:48:12 -0400

    Initial Commit

---
+++ Filmy/FilmyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmyApp.swift
+//  Filmy
+//
+//  Created by Nikita Zhdanov on 2024-05-29.
+//
+
+import SwiftUI
+
+@main
+struct FilmyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
