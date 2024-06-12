bac3b42366e8a62958d6c25f701dfaa8262fa1b7
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-06-12 12:11:57 -0400

    Cleared my code from useless parts

--- Filmy/Model/MovieDataModel.swift
+++ Filmy/Model/MovieDataModel.swift
@@ -30,7 +30,6 @@ class MovieDetails {
     let title: String
     let duration_m: Int
     let date_released: String
-    let budget: Int
     let description: String
     let genres: [String]
     var platformsToWatch: [String]
@@ -43,11 +42,10 @@ class MovieDetails {
     
     
     
-    init(title: String, duration_m: Int, date_released: String, budget: Int, description: String, genres: [String], platformsToWatch: [String], actorsInvolved: [String], imbdRating: Double, status: String, isInLibrary: Bool, userLiked: Bool) {
+    init(title: String, duration_m: Int, date_released: String, description: String, genres: [String], platformsToWatch: [String], actorsInvolved: [String], imbdRating: Double, status: String, isInLibrary: Bool, userLiked: Bool) {
         self.title = title
         self.duration_m = duration_m
         self.date_released = date_released
-        self.budget = budget
         self.description = description
         self.genres = genres
         self.platformsToWatch = platformsToWatch
@@ -58,32 +56,14 @@ class MovieDetails {
         self.userLiked = userLiked
     }
 }
-
-
-@Observable
-class movieSuggestion {
-    var mainMovie: MovieDetails
-    
-    var movieIfNo: MovieDetails
-    
-    var movieIfYes: MovieDetails
-    
-    init(mainMovie: MovieDetails, movieIfNo: MovieDetails, movieIfYes: MovieDetails) {
-        self.mainMovie = mainMovie
-        self.movieIfNo = movieIfNo
-        self.movieIfYes = movieIfYes
-    }
-}
     
 
-var DunePartTwo = MovieDetails(title: "Dune: Part Two", duration_m: 166, date_released: "2024", budget: 190000000, description: "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the universe, he must prevent a terrible future only he can foresee.", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Zendaya", "Timothee Chalet"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
+var DunePartTwo = MovieDetails(title: "Dune: Part Two", duration_m: 166, date_released: "2024", description: "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the universe, he must prevent a terrible future only he can foresee.", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Zendaya", "Timothee Chalet"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
 
-var Batman = MovieDetails(title: "Batman", duration_m: 142, date_released: "2023", budget: 113400000, description: "Some description", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Batman", "Joker"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
+var Batman = MovieDetails(title: "Batman", duration_m: 142, date_released: "2023", description: "Some description", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Batman", "Joker"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
 
-var Interstellar = MovieDetails(title: "Interstellar", duration_m: 194, date_released: "2014", budget: 120000000, description: "Some description", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Cosmonaut", "Tony Stark"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
+var Interstellar = MovieDetails(title: "Interstellar", duration_m: 194, date_released: "2014", description: "Some description", genres: ["Sci-fi", "Fantasy", "Drama", "Action"], platformsToWatch: ["Netflix", "Disney", "PrimeVideo"], actorsInvolved: ["Cosmonaut", "Tony Stark"], imbdRating: 9.3, status: "Not wathced", isInLibrary: false, userLiked: false)
     
     
 
 var moviesList: [MovieDetails] = [DunePartTwo, Batman, Interstellar, DunePartTwo, Batman, Interstellar, DunePartTwo, Batman, Interstellar]
-
-var suggestedMovies = movieSuggestion(mainMovie: DunePartTwo, movieIfNo: Batman, movieIfYes: Interstellar)
